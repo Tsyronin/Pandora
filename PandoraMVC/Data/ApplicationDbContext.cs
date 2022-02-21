4f8265a7501e85e47ecf7a35731797693517c50d
Pavlo <pavelcrn11@gmail.com>
2022-02-21 21:57:16 +0300

    Adds database configurations
    Adds EF entities, creates context and adds migrations.

--- PandoraMVC/Data/ApplicationDbContext.cs
+++ PandoraMVC/Data/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PandoraMVC.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PandoraMVC.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Epic> Epics { get; set; }
+        public DbSet<Workspace> Workspaces { get; set; }
     }
 }
